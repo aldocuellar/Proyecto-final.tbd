7cb5ab5343adb462cc13cca58d4a93e2562dfc59
aldocuellar <60556844+aldocuellar@users.noreply.github.com>
2020-08-20 03:27:01 -0700

    Change 4

--- Proyecto Final.tbd/Proyecto Final.tbd/Form1.Designer.cs
+++ Proyecto Final.tbd/Proyecto Final.tbd/Form1.Designer.cs
@@ -142,14 +142,17 @@
             this.btnEntradas.FlatAppearance.BorderSize = 0;
             this.btnEntradas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnEntradas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEntradas.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntradas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEntradas.ForeColor = System.Drawing.Color.White;
+            this.btnEntradas.Image = ((System.Drawing.Image)(resources.GetObject("btnEntradas.Image")));
+            this.btnEntradas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnEntradas.Location = new System.Drawing.Point(0, 320);
             this.btnEntradas.Name = "btnEntradas";
             this.btnEntradas.Size = new System.Drawing.Size(196, 40);
             this.btnEntradas.TabIndex = 7;
             this.btnEntradas.Text = "Entradas";
             this.btnEntradas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEntradas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnEntradas.UseVisualStyleBackColor = true;
             this.btnEntradas.Click += new System.EventHandler(this.btnEntradas_Click);
             // 
@@ -159,14 +162,17 @@
             this.btnDV.FlatAppearance.BorderSize = 0;
             this.btnDV.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnDV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDV.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDV.ForeColor = System.Drawing.Color.White;
+            this.btnDV.Image = ((System.Drawing.Image)(resources.GetObject("btnDV.Image")));
+            this.btnDV.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnDV.Location = new System.Drawing.Point(0, 280);
             this.btnDV.Name = "btnDV";
             this.btnDV.Size = new System.Drawing.Size(196, 40);
             this.btnDV.TabIndex = 6;
             this.btnDV.Text = "Detalle Venta";
             this.btnDV.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDV.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnDV.UseVisualStyleBackColor = true;
             this.btnDV.Click += new System.EventHandler(this.btnDV_Click);
             // 
@@ -176,14 +182,17 @@
             this.btnMP.FlatAppearance.BorderSize = 0;
             this.btnMP.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnMP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMP.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMP.ForeColor = System.Drawing.Color.White;
+            this.btnMP.Image = ((System.Drawing.Image)(resources.GetObject("btnMP.Image")));
+            this.btnMP.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnMP.Location = new System.Drawing.Point(0, 240);
             this.btnMP.Name = "btnMP";
             this.btnMP.Size = new System.Drawing.Size(196, 40);
             this.btnMP.TabIndex = 5;
             this.btnMP.Text = "Marca";
             this.btnMP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMP.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnMP.UseVisualStyleBackColor = true;
             this.btnMP.Click += new System.EventHandler(this.btnMP_Click);
             // 
@@ -193,14 +202,16 @@
             this.btnVenta.FlatAppearance.BorderSize = 0;
             this.btnVenta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVenta.ForeColor = System.Drawing.Color.White;
+            this.btnVenta.Image = ((System.Drawing.Image)(resources.GetObject("btnVenta.Image")));
+            this.btnVenta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnVenta.Location = new System.Drawing.Point(0, 200);
             this.btnVenta.Name = "btnVenta";
             this.btnVenta.Size = new System.Drawing.Size(196, 40);
             this.btnVenta.TabIndex = 4;
             this.btnVenta.Text = "Venta";
-            this.btnVenta.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVenta.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnVenta.UseVisualStyleBackColor = true;
             this.btnVenta.Click += new System.EventHandler(this.btnVenta_Click);
             // 
@@ -210,14 +221,16 @@
             this.btnFP.FlatAppearance.BorderSize = 0;
             this.btnFP.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnFP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFP.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFP.ForeColor = System.Drawing.Color.White;
+            this.btnFP.Image = ((System.Drawing.Image)(resources.GetObject("btnFP.Image")));
+            this.btnFP.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnFP.Location = new System.Drawing.Point(0, 160);
             this.btnFP.Name = "btnFP";
             this.btnFP.Size = new System.Drawing.Size(196, 40);
             this.btnFP.TabIndex = 3;
             this.btnFP.Text = "Familia";
-            this.btnFP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFP.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnFP.UseVisualStyleBackColor = true;
             this.btnFP.Click += new System.EventHandler(this.btnFP_Click);
             // 
@@ -227,14 +240,17 @@
             this.btnTP.FlatAppearance.BorderSize = 0;
             this.btnTP.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnTP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTP.ForeColor = System.Drawing.Color.White;
+            this.btnTP.Image = ((System.Drawing.Image)(resources.GetObject("btnTP.Image")));
+            this.btnTP.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnTP.Location = new System.Drawing.Point(0, 120);
             this.btnTP.Name = "btnTP";
             this.btnTP.Size = new System.Drawing.Size(196, 40);
             this.btnTP.TabIndex = 2;
             this.btnTP.Text = "Tipo Producto";
             this.btnTP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTP.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnTP.UseVisualStyleBackColor = true;
             this.btnTP.Click += new System.EventHandler(this.btnTP_Click);
             // 
@@ -244,14 +260,17 @@
             this.btnTU.FlatAppearance.BorderSize = 0;
             this.btnTU.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnTU.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTU.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTU.ForeColor = System.Drawing.Color.White;
+            this.btnTU.Image = ((System.Drawing.Image)(resources.GetObject("btnTU.Image")));
+            this.btnTU.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnTU.Location = new System.Drawing.Point(0, 80);
             this.btnTU.Name = "btnTU";
             this.btnTU.Size = new System.Drawing.Size(196, 40);
             this.btnTU.TabIndex = 1;
             this.btnTU.Text = "Tipo Usuario";
-            this.btnTU.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTU.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTU.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnTU.UseVisualStyleBackColor = true;
             this.btnTU.Click += new System.EventHandler(this.btnTU_Click);
             // 
@@ -261,14 +280,17 @@
             this.btnProductos.FlatAppearance.BorderSize = 0;
             this.btnProductos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProductos.ForeColor = System.Drawing.Color.White;
+            this.btnProductos.Image = ((System.Drawing.Image)(resources.GetObject("btnProductos.Image")));
+            this.btnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnProductos.Location = new System.Drawing.Point(0, 40);
             this.btnProductos.Name = "btnProductos";
             this.btnProductos.Size = new System.Drawing.Size(196, 40);
             this.btnProductos.TabIndex = 0;
             this.btnProductos.Text = "Productos";
-            this.btnProductos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnProductos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnProductos.UseVisualStyleBackColor = true;
             this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
             // 
@@ -278,14 +300,17 @@
             this.btnUsuarios.FlatAppearance.BorderSize = 0;
             this.btnUsuarios.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
             this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUsuarios.ForeColor = System.Drawing.Color.White;
+            this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
+            this.btnUsuarios.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnUsuarios.Location = new System.Drawing.Point(0, 0);
             this.btnUsuarios.Name = "btnUsuarios";
             this.btnUsuarios.Size = new System.Drawing.Size(196, 40);
             this.btnUsuarios.TabIndex = 0;
             this.btnUsuarios.Text = "Usuarios";
-            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnUsuarios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnUsuarios.UseVisualStyleBackColor = true;
             this.btnUsuarios.Click += new System.EventHandler(this.btnUsuarios_Click);
             // 
